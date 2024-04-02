d77338fd18babf743164bbd5357673d042eb88b5
ImBenjaminKaiser <bnkaiser04@gmail.com>
2024-04-01 21:18:58 -0400

    added individual JS and CSS files for different pages as well as global CSS and JS files for pages. Also added code to respect prefers-reduced-motion on 5 qualities page

--- js/global.js
+++ js/global.js
@@ -1,32 +1,36 @@
 // This file is so I don't have to repeat this code in every JS file
 
-// Register ScrollTrigger plugin
-// Typically done automatically but best practice to manually register it
-gsap.registerPlugin(ScrollTrigger);
+// Fixes the GSAP not defined error
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Register ScrollTrigger plugin
+    // Typically done automatically but best practice to manually register it
+    gsap.registerPlugin(ScrollTrigger);
 
 
-const isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
-// For respecting prefers reduced motion preference; from here: https://natclark.com/tutorials/javascript-reduced-motion/
+    const isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+    // For respecting prefers reduced motion preference; from here: https://natclark.com/tutorials/javascript-reduced-motion/
 
-// Only apply Lenis smooth scrolling if the user has not set prefers reduced motion to reduce
-if (isReduced === false){
-    
-    // From lenis docs: https://github.com/darkroomengineering/lenis?tab=readme-ov-file#gsap-scrolltrigger
-    // Initializes a Lenis object that makes scrolling smoother
-    const lenis = new Lenis({
-        duration:1.5,// How long it takes to catch up with the actual scroll position
-        wheelMultiplier: 1.3,// Makes the user scroll faster
-        smooth: true,
-    });
+    // Only apply Lenis smooth scrolling if the user has not set prefers reduced motion to reduce
+    if (isReduced === false){
+        
+        // From lenis docs: https://github.com/darkroomengineering/lenis?tab=readme-ov-file#gsap-scrolltrigger
+        // Initializes a Lenis object that makes scrolling smoother
+        const lenis = new Lenis({
+            duration:1.5,// How long it takes to catch up with the actual scroll position
+            wheelMultiplier: 1.3,// Makes the user scroll faster
+            smooth: true,
+        });
 
-    lenis.on('scroll', (e) => {
-    //   console.log(e) // Shows information about the current scrolling state
-    });
+        lenis.on('scroll', (e) => {
+        //   console.log(e) // Shows information about the current scrolling state
+        });
 
-    lenis.on('scroll', ScrollTrigger.update);
+        lenis.on('scroll', ScrollTrigger.update);
 
-    gsap.ticker.add((time)=>{
-    lenis.raf(time * 1000)
-    });
+        gsap.ticker.add((time)=>{
+        lenis.raf(time * 1000)
+        });
 
-}
+    }
+})
--- js/homepage.js
+++ js/homepage.js
@@ -1,15 +1,12 @@
-// const header = document.querySelector("#hero-header-text")
-// const text = new SplitType(header, { types: 'letters' });
-
-// console.log(text.words)
-
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector(".hero-header-text");
     const text = new SplitType(header, { types: 'words' });
-    console.log(text.words);
+    // console.log(text.words);
     gsap.from(text.words,{
         y:1000,
         stagger:.1,
-        ease: "power4.out",
+        ease: "expo.out",
+        duration:.7,
     })
+    // gsap.from(".hero-header-wrapper",{backgroundSize: (i, e) => bgSize(150%)})
 });

a1f925b4a2b12156737590826665333f94eeb3b6
ImBenjaminKaiser <bnkaiser04@gmail.com>
2024-04-06 21:10:30 -0400

    started experiences page, various other changes

--- js/codingprogression.js
+++ js/codingprogression.js
@@ -1,26 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Make code blocks with  HighlightJS
     hljs.highlightAll()
-    const wave1dur = (gsap.utils.random(27,30)/(window.innerWidth/(window.innerWidth/2)))
-    const wave2dur = (gsap.utils.random(24,27)/(window.innerWidth/(window.innerWidth/2)))
-    const wave3dur = (gsap.utils.random(19,21)/(window.innerWidth/(window.innerWidth/2)))
-    const wave4dur = (gsap.utils.random(16,19)/(window.innerWidth/(window.innerWidth/2)))
-    const wave5dur = (gsap.utils.random(13,16)/(window.innerWidth/(window.innerWidth/2)))
-
-    gsap.set(".wave1",{x:(2500-(window.innerWidth/2))})
-    gsap.set(".wave2",{x:(2500-(window.innerWidth/2))})
-    gsap.set(".wave3",{x:(2500-(window.innerWidth/2))})
-    gsap.set(".wave4",{x:(2500-(window.innerWidth/2))})
-    gsap.set(".wave5",{x:(2500-(window.innerWidth/2))})
-    // If the screen width times 2.5 minus 5000 is not a negative number, then make x 0
-    var xAmount = -(5000-(window.innerWidth*2.5));
-    if (xAmount > 0){
-    xAmount = 0;
-    }
-
-
-gsap.to(".wave1",{x:xAmount,duration:wave1dur,repeat:-1,yoyo:true,ease:'none'})
-gsap.to(".wave2",{x:xAmount,duration:wave2dur,repeat:-1,yoyo:true,ease:'none'})
-gsap.to(".wave3",{x:xAmount,duration:wave3dur,repeat:-1,yoyo:true,ease:'none'})
-gsap.to(".wave4",{x:xAmount,duration:wave4dur,repeat:-1,yoyo:true,ease:'none'})
-gsap.to(".wave5",{x:xAmount,duration:wave5dur,repeat:-1,yoyo:true,ease:'none'})
+        // They are centered on the page because of the wrapper so this moves the left edge of the waves to the left edge of the screen
+        gsap.set(".wave1",{x:(2500-(window.innerWidth/2))})
+        gsap.set(".wave2",{x:(2500-(window.innerWidth/2))})
+        gsap.set(".wave3",{x:(2500-(window.innerWidth/2))})
+        gsap.set(".wave4",{x:(2500-(window.innerWidth/2))})
+        gsap.set(".wave5",{x:(2500-(window.innerWidth/2))})
 })
--- js/global.js
+++ js/global.js
@@ -32,5 +32,33 @@ document.addEventListener('DOMContentLoaded', () => {
         lenis.raf(time * 1000)
         });
 
+        // Check to see if page has waves footer
+        const hasFooter = document.querySelector('.wave1');
+        // If not, don't animate it
+        if (hasFooter !== null) {
+
+        // Random Durations for wave animations, get faster if the user is zoomed out more
+        const wave1dur = (gsap.utils.random(27,30)/(window.innerWidth/(window.innerWidth/2)))
+        const wave2dur = (gsap.utils.random(24,27)/(window.innerWidth/(window.innerWidth/2)))
+        const wave3dur = (gsap.utils.random(19,21)/(window.innerWidth/(window.innerWidth/2)))
+        const wave4dur = (gsap.utils.random(16,19)/(window.innerWidth/(window.innerWidth/2)))
+        const wave5dur = (gsap.utils.random(13,16)/(window.innerWidth/(window.innerWidth/2)))
+    
+
+
+        // If the screen width times 2.5 minus 5000 is not a negative number, then make x 0, causing the waves to not animate
+        var xAmount = -(5000-(window.innerWidth*2.5));
+        if (xAmount > 0){
+        xAmount = 0;
+        }
+
+        // Animate the waves, repeat forever, and yoyo (reverse) the animation on complete
+        gsap.to(".wave1",{x:xAmount,duration:wave1dur,repeat:-1,yoyo:true,ease:'none'})
+        gsap.to(".wave2",{x:xAmount,duration:wave2dur,repeat:-1,yoyo:true,ease:'none'})
+        gsap.to(".wave3",{x:xAmount,duration:wave3dur,repeat:-1,yoyo:true,ease:'none'})
+        gsap.to(".wave4",{x:xAmount,duration:wave4dur,repeat:-1,yoyo:true,ease:'none'})
+        gsap.to(".wave5",{x:xAmount,duration:wave5dur,repeat:-1,yoyo:true,ease:'none'})
+
+        }
     }
 })
